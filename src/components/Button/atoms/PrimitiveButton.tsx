267dc93887289c3611deaa7987601ec6dea9aa63
Paweł Sieniewicz <pawel.sieniewicz@gmail.com>
2024-04-25 22:32:23 +0200

    Added button transition

--- src/components/Button/atoms/PrimitiveButton.tsx
+++ src/components/Button/atoms/PrimitiveButton.tsx
@@ -1,24 +1,28 @@
 import styled from "styled-components";
 
 export const PrimitiveButton = styled.button`
-  background-color: ${({ theme }) => theme.colors.primary.main};
-  color: ${({ theme }) => theme.colors.white};
-  border: none;
-  padding: ${({ theme }) => theme.spacing.sm} ${({ theme }) => theme.spacing.md};
-  border-radius: 8px;
-  font-size: ${({ theme }) => theme.fontSize.default};
-  font-family: ${({ theme }) => theme.fontFamily.montserrat};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: ${({ theme }) => theme.spacing.xsm};
+    background-color: ${({theme}) => theme.colors.primary.main};
+    color: ${({theme}) => theme.colors.white};
+    border: none;
+    padding: ${({theme}) => theme.spacing.sm} ${({theme}) => theme.spacing.md};
+    border-radius: 8px;
+    font-size: ${({theme}) => theme.fontSize.default};
+    font-family: ${({theme}) => theme.fontFamily.montserrat};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: ${({theme}) => theme.spacing.xsm};
 
-  svg {
-    height: ${({ theme }) => theme.icon.default};
-    width: auto;
-  }
+    transition-property: color, background-color, border-color, text-decoration-color, fill, stroke;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 0.15s;
 
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.primary.gentle};
-  }
+    svg {
+        height: ${({theme}) => theme.icon.default};
+        width: auto;
+    }
+
+    &:hover {
+        background-color: ${({theme}) => theme.colors.primary.gentle};
+    }
 `;
