6a872975166889a446b25d531877ece0401a41d9
Paweł Sieniewicz <pawel.sieniewicz@gmail.com>
2024-04-30 02:38:20 +0200

    Style fix of the EmptyResult component

--- src/components/EmptyResults.tsx
+++ src/components/EmptyResults.tsx
@@ -3,22 +3,17 @@ import styled from 'styled-components';
 import { EmptySearchIcon } from './Icons';
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: ${({ theme }) => theme.colors.primary.subtle};
-
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
-  svg {
-    height: 36px;
-    width: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     color: ${({ theme }) => theme.colors.primary.subtle};
-  }
+
+    svg {
+        height: 36px;
+        width: auto;
+        color: ${({ theme }) => theme.colors.primary.subtle};
+    }
 `;
 
 export const EmptyResults = () => {
